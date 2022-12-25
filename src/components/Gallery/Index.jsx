1f682573a62b2176f51f43e7dfba14abceba1523
plfmoura <plfmoura96@gmail.com>
2022-12-25 11:52:40 -0300

    alter(main)gallery card-title

--- src/components/Gallery/Index.jsx
+++ src/components/Gallery/Index.jsx
@@ -9,9 +9,9 @@ export default function Gallery() {
             <GalleryCard title={"Animais"} identificador={1} />
             <GalleryCard title={"Pessoas"} identificador={2}/>
             <GalleryCard title={"Cidades"} identificador={3}/>
-            <GalleryCard title={"Arquitetura"} identificador={4}/>
-            <GalleryCard title={"Animais"} identificador={5}/>
-            <GalleryCard title={"Pessoas"} identificador={6}/>
+            <GalleryCard title={"Decoração"} identificador={4}/>
+            <GalleryCard title={"Arquitetura"} identificador={5}/>
+            <GalleryCard title={"Natureza"} identificador={6}/>
         </div>
     </main>
   )
